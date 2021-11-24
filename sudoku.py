72de9964dab448cffd171b129bc7055b9837a604
Divakar choudhary <54638339+choudhary2001@users.noreply.github.com>
2021-11-25 00:44:15 +0530

    Create sudoku.py

---
+++ sudoku.py
@@ -0,0 +1,56 @@
+def find_next_empty(puzzle):
+    for r in range(9):
+        for c in range(9):
+            if puzzle[r][c] == -1:
+                return r,c
+
+    return None, None
+
+def is_valid(puzzle, guess, row, col):
+    row_vals = puzzle[row]
+    if guess in row_vals:
+        return False
+
+    col_vals = [puzzle[i][col] for i in range(9)]
+    if guess in col_vals:
+        return False
+
+    row_start = (row // 3) * 3
+    col_start = (col // 3) * 3
+
+    for r in range(row_start, row_start + 3):
+        for c in range(col_start, col_start + 3):
+            if puzzle[r][c] == guess:
+                return False
+        
+    return True
+    
+
+def solve_sudoku(puzzle):
+    row, col = find_next_empty(puzzle)
+
+    if row is None:
+        return True
+
+    for guess in range(1, 10):
+        if is_valid(puzzle, guess, row, col):
+            puzzle[row][col] = guess
+
+            if solve_sudoku(puzzle):
+                return True
+
+        puzzle[row][col] = -1
+
+    return False
+
+if __name__ == '__main__':
+    example_board = [ ]
+    print("Sudoko is 9 x 9.\nEnter one number and press enter >>>> \n")
+    for i in range(9):
+         a = [int(input()), int(input()), int(input()), int(input()), int(input()), int(input()), int(input()), int(input()), int(input())]
+         example_board.append(a)
+            
+    
+    print(example_board)
+    print(solve_sudoku(example_board))
+    print(example_board)
